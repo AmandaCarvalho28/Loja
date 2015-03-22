92debb8cd6182ed93db5e908ba446d127743edf5
AmandaCarvalho28 <amanda.chrys370@hotmail.com>
2015-03-22 03:20:09 -0300

    Commit inicial

---
+++ Carvalho.LojaVirtual.UnitTest/UnitTestCarvalho.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Carvalho.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestCarvalho
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
